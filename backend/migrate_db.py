eea2a55bfaaa50fc88162dc5a3cbf1e5c0c9a61b
Joel Kinny <joelkny97@gmail.com>
2024-10-10 09:42:02 -0400

    migration db script and hosting server

---
+++ backend/migrate_db.py
@@ -0,0 +1,3 @@
+!python backend/manage.py runserver makemigrations
+!python backend/manage.py runserver migrate
+!python backend/manage.py collectstatic
--- backend/run_django_app.py
+++ backend/run_django_app.py
@@ -1,1 +1,1 @@
-!python backend/manage.py runserver 0.0.0.0:3000
+!python backend/manage.py runserver --bind 127.0.0.1:3000
