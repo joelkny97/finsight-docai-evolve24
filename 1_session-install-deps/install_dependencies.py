3afd3c1fd9427839e07907181c673077d3640895
Joel Kinny <joelkny97@gmail.com>
2024-10-10 01:44:13 -0400

    start server fix

--- 1_session-install-deps/install_dependencies.py
+++ 1_session-install-deps/install_dependencies.py
@@ -2,5 +2,6 @@
 !source venv/bin/activate
 !pip install --upgrade pip
 !pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -U --no-deps "feedparser>=6.0.8" -r 1_session-install-deps/requirements.txt
-!cd ../frontend && npm install --log 1_session-install-deps/npm-req.log
+!cd frontend
+!npm install --log 1_session-install-deps/npm-req.log
 
