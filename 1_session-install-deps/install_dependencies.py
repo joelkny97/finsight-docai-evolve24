8d3d8c6f5bc5dfd8125d448f576ac84882df9971
Joel Kinny <joelkny97@gmail.com>
2024-10-10 00:17:22 -0400

    changes to package deployment scripts

--- 1_session-install-deps/install_dependencies.py
+++ 1_session-install-deps/install_dependencies.py
@@ -1,4 +1,6 @@
+!python -m venv venv
+!source venv/bin/activate
 !pip install --upgrade pip
-!pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -r 1_session-install-deps/requirements.txt
-!pip install -U --no-deps "feedparser>=6.0.8"
-!npm install 1_session-install-deps/package.json --log 1_session-install-deps/npm-req.log
+!pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -U --no-deps "feedparser>=6.0.8" -r 1_session-install-deps/requirements.txt
+!cd ../frontend && npm install --log 1_session-install-deps/npm-req.log
+
